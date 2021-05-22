5e18df057cf726d6f9b3b3ae1721fbd07f6396cc
unknown <lukanevteach@gmail.com>
2021-05-22 14:34:26 +0300

    edit files HappyArray.kt and BillCOunter.kt

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val list = sadArray.toMutableList()
+        var i: Int
+        var counterSadElem: Int
+        do {
+            counterSadElem = 0
+            i = 1
+            while (i < list.size - 1) {
+                if (list[i] > (list[i - 1] + list[i + 1])) {
+                    list.removeAt(i)
+                    counterSadElem++
+                    i--
+                }
+                i++
+            }
+        } while (counterSadElem != 0)
+        return list.toIntArray()
     }
 }
