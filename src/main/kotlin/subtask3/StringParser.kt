81a745f11cdef243d617dbb621533a0ad87d8405
unknown <lukanevteach@gmail.com>
2021-05-23 12:08:32 +0300

    Edit file StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -9,17 +9,36 @@ class StringParser {
     return list.toTypedArray()
     }
     fun getParsers(str : String) : MutableList<String> {
-        val list : MutableList<String> = mutableListOf<String>()
-        val p1 = Regex("""\(.+?\)|\[.+?\]|<.+?>""")
-        val parsers : Sequence<MatchResult> = p1.findAll(str, 0)
+//        val list : MutableList<String> = mutableListOf<String>()
+//        val p1 = Regex("""\(.+?\(.+?\).+?\)|\[.+?\[.+?\].+?\]|<.+?<.+?>.+?>|\(.+?\)|\[.+?\]|<.+?>""")
+//        val parsers : Sequence<MatchResult> = p1.findAll(str, 0)
+//        parsers.forEach()
+//        {
+//                matchResult ->
+//            var parser = matchResult.value.toString().subSequence(1,matchResult.value.length-1).toString()
+//            list.add(parser)
+//            if (p1.containsMatchIn(str)){
+//                list.addAll(getParsers(parser))
+//            }
+//        }
+//        return list
+        val list = mutableListOf<String>()
+        val p1 = Regex("""<.+?>""")
+        val p2 = Regex("""<.+?<.+?>.+?>|<.+?>|\(.+?\(.+?\).+?\)|\[.+?\[.+?].+?]|\(.+?\)|\[.+?]""")
+        val parsers : Sequence<MatchResult> = p2.findAll(str, 0)
         parsers.forEach()
         {
                 matchResult ->
-            var parser = matchResult.value.toString().subSequence(1,matchResult.value.length-1).toString()
+            var parser = matchResult.value.subSequence(1,matchResult.value.length-1).toString()
             list.add(parser)
-            if (p1.containsMatchIn(str)){
+            if (p1.containsMatchIn(parser)){
+                var pr = p1.find(parser,0)
+                list.add(pr?.value?.subSequence(1,pr.value.length-1).toString())
+            }
+            if (p2.containsMatchIn(parser)){
                 list.addAll(getParsers(parser))
             }
+
         }
         return list
     }
