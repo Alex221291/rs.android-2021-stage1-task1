c0a7fc982b89112b2797aadcf5c41bdc871e59fd
unknown <lukanevteach@gmail.com>
2021-05-23 09:38:51 +0300

    Edit file StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,23 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var list : MutableList<String> = mutableListOf<String>()
+        list = getParsers(inputString)
+    return list.toTypedArray()
+    }
+    fun getParsers(str : String) : MutableList<String> {
+        val list : MutableList<String> = mutableListOf<String>()
+        val p1 = Regex("""\(.+?\)|\[.+?\]|<.+?>""")
+        val parsers : Sequence<MatchResult> = p1.findAll(str, 0)
+        parsers.forEach()
+        {
+                matchResult ->
+            var parser = matchResult.value.toString().subSequence(1,matchResult.value.length-1).toString()
+            list.add(parser)
+            if (p1.containsMatchIn(str)){
+                list.addAll(getParsers(parser))
+            }
+        }
+        return list
     }
 }
